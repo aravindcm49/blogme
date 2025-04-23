23e8e77678f8a33eb44ae29120ebf0c51ce2563b
aravindcm49 <aravindcm49@gmail.com>
2025-04-23 09:55:00 +0530

    🎨 Added astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel({
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+      domains: [],
+      remotePatterns: [],
+    },
+	imageService: true,
+	devImageService: 'sharp'
+  }),
 });
